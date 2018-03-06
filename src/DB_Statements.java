b88177b184e7d745d6c8570ec4bfcb13c44832a3
dani832m <31951102+dani832m@users.noreply.github.com>
2018-03-06 14:10:38 +0100

    DB_Statements etc. created

---
+++ src/DB_Statements.java
@@ -0,0 +1,24 @@
+//Necessary imports
+import java.sql.*;
+import java.io.*;
+
+//Public class for DB_Statements
+public class DB_Statements {
+
+    //Declare a Statement
+    private static Statement stmt = null;
+
+    //Declare and create a Connection
+    private static Connection con = DB_Connector.connect();
+
+    //Declare a Result Set
+    private static ResultSet rs = null;
+
+    //Declare a PreparedStatement
+    private static PreparedStatement pst = null;
+
+    public void insertData() {
+
+
+    }
+}
---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+//Public class for Employee
+public class Employee {
+
+    //Instance variables
+    private int id;
+    private String name;
+    private double salary;
+
+    //Constructor with 3 parameters
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+}
